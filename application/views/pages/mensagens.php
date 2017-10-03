89580ce2f6bd1555140897b42d6d864a78059baf
Vinicius Fernando <vihh.fernando@gmail.com>
2017-10-03 14:20:59 -0300

    detalhes chat

--- application/views/pages/mensagens.php
+++ application/views/pages/mensagens.php
@@ -1,6 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <script>
     var Cliente = '<?php echo $view->item( 'cliente' )->CodCliente; ?>';
+    var mensagens = '<?php echo count( $view->item( 'mensagens' ) ); ?>';
 </script>
 <?php $view->component( 'navbar/navbar' ); ?>
 <div class="container-fluid">
@@ -16,9 +17,9 @@
                 </h3>
             </div><!-- cabecalho -->
 
-            <div class="mensagens-wrapper col-md-12">
+            <div id="mensagens" class="mensagens-wrapper col-md-12">
 
-                <?php foreach( $view->item( 'mensagens' ) as $mensagem ):?>
+                <?php foreach( $view->item( 'mensagens' ) as $index => $mensagem ):?>
                 <div class="media chat-bubble <?php echo $mensagem->funcionario ? 'sent' : ''; ?>">
                     <div class="media-body">
                         <?php if( isset( $mensagem->arquivo ) ) : ?>
@@ -27,7 +28,7 @@
                             <?php echo $mensagem->label;?>
                         </a>
                         <?php else : ?>
-                            <?php echo $mensagem->visualizada == 'N' ? '<b>' .$mensagem->texto .'</b>' : $mensagem->texto; ?>
+                            <?php echo $mensagem->visualizada == 'N' && $mensagem->autor != 'F' ? '<b>' .$mensagem->texto .'</b>' : $mensagem->texto; ?>
                         <?php endif; ?>
                         <small style="display: block; margin-top: 10px;">
                             <i>Enviado em <?php echo date( 'd/m/Y \à\s H:i' ); ?>
--- assets/pages/mensagens/mensagens.js
+++ assets/pages/mensagens/mensagens.js
@@ -201,4 +201,38 @@ $( document ).ready( function() {
         // adiciona o arquivo na lista
         adicionarArquivoNaLista( $( this ), atualizarProgresso, finalizarUpload );
     });
+    $('#mensagens').animate({
+        scrollTop: $(this).height() + mensagens * 100 // aqui introduz o numero de px que quer no scroll, neste caso é a altura da propria div, o que faz com que venha para o fim
+    }, 0);
+});
+
+function getCaret(el) { 
+    if (el.selectionStart) { 
+        return el.selectionStart; 
+    } else if (document.selection) { 
+        el.focus();
+        var r = document.selection.createRange(); 
+        if (r == null) { 
+            return 0;
+        }
+        var re = el.createTextRange(), rc = re.duplicate();
+        re.moveToBookmark(r.getBookmark());
+        rc.setEndPoint('EndToStart', re);
+        return rc.text.length;
+    }  
+    return 0; 
+}
+
+$('textarea').keyup(function (event) {
+    if (event.keyCode == 13) {
+        var content = this.value;  
+        var caret = getCaret(this);          
+        if(event.shiftKey){
+            this.value = content.substring(0, caret - 1) + "\n" + content.substring(caret, content.length);
+            event.stopPropagation();
+        } else {
+            this.value = content.substring(0, caret - 1) + content.substring(caret, content.length);
+            $('form').submit();
+        }
+    }
 });
